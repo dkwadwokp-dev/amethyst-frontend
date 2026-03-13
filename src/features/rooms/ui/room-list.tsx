868a74f62564f091d949d831e93bf7276492353b
Asare Foster <fostersoasare@gmail.com>
2026-03-13 02:53:03 +0000

    Mobile responsiveness completed

--- src/features/rooms/ui/room-list.tsx
+++ src/features/rooms/ui/room-list.tsx
@@ -12,12 +12,16 @@ const RoomList = () => {
         {rooms.map((room) => (
           <div
             key={room.id}
-            className="bg-white p-4 md:p-6 flex flex-col md:flex-row gap-8 shadow-sm items-center"
+            className="bg-white p-4 md:p-6 flex flex-col md:flex-row gap-4 md:gap-8 shadow-sm items-center"
           >
             <div className="w-full md:w-[35%] shrink-0">
-              <ImagePlaceholder className="w-full h-56 min-h-[220px]" src={room.leadImage} text={room.title} />
+              <ImagePlaceholder
+                className="w-full h-56 min-h-[220px]"
+                src={room.leadImage}
+                text={room.title}
+              />
             </div>
-            <div className="flex-1 py-4 flex flex-col md:flex-row justify-between gap-6 h-full">
+            <div className="flex-1 flex flex-col md:flex-row justify-between gap-6 h-full">
               <div className="flex-1 flex flex-col justify-between">
                 <div>
                   <h3 className="text-xl font-marcellus text-primary uppercase mb-3">
@@ -41,7 +45,10 @@ const RoomList = () => {
                   ${room.price}/NIGHT
                 </div>
                 <div className="w-full mt-6 md:mt-0 flex flex-col items-start md:items-end justify-end flex-1">
-                  <Link to={`/book?room=${room.id}`} className="w-full block mb-3">
+                  <Link
+                    to={`/book?room=${room.id}`}
+                    className="w-full block mb-3"
+                  >
                     <Button
                       variant="primary"
                       className="w-full bg-[#2A2E33] hover:bg-black text-white px-6 py-3 text-[10px] tracking-widest rounded-none border-none shadow-none"
@@ -49,7 +56,10 @@ const RoomList = () => {
                       BOOK NOW
                     </Button>
                   </Link>
-                  <Link to={`/rooms/${room.id}`} className="text-[10px] text-gray-500 uppercase tracking-widest hover:text-gray-900 cursor-pointer text-center w-full md:text-right">
+                  <Link
+                    to={`/rooms/${room.id}`}
+                    className="text-[10px] text-gray-500 uppercase tracking-widest hover:text-gray-900 cursor-pointer text-center w-full md:text-right"
+                  >
                     VIEW DETAILS
                   </Link>
                 </div>
